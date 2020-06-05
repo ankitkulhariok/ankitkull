734dd35520de50f8ec32ed4231811808ca162016
ankitkulhariok <66436620+ankitkulhariok@users.noreply.github.com>
2020-06-05 08:13:34 +0530

    Update indianbot_string_setup.py

--- indianbot_string_setup.py
+++ indianbot_string_setup.py
@@ -11,7 +11,7 @@ print("""Please go-to my.telegram.org
 Login using your Telegram account
 Click on API Development Tools
 Create a new application, by entering the required details
-if any problem contact at @IndianBot_Official on telegram""")
+if any problem contact at @Lazykilller on telegram""")
 APP_ID = int(input("Enter APP ID here: "))
 API_HASH = input("Enter API HASH here: ")
 
